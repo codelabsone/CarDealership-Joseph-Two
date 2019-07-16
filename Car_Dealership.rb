d36e2c3970db56d4d7a9dd24a303a6c960023e6e
Garr22 <bohnertgarrett@gmail.com>
2019-07-15 20:02:27 -0700

    Hi!

--- Car_Dealership.rb
+++ Car_Dealership.rb
@@ -1,7 +1,7 @@
 puts "Car dealership"
 <<<<<<< HEAD
-Car = "year, car_name"
+Car = "Make_of_Car, car_name"
 =======
-car = "car_name"
-make = "Make_of_car"
+car = "Car_Name"
+make = "Make_of_Car"
 >>>>>>> 1e0184b8eb0f7d5f9c18ddfaa26ccf89fa23be84
