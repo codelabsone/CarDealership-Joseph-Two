237d225f1acea8ab99a226c2890b07c7903cda9d
ndschalk <52681861+ndschalk@users.noreply.github.com>
2019-07-18 19:56:22 -0500

    added status

--- Car_Dealership.rb
+++ Car_Dealership.rb
@@ -1,17 +1,18 @@
 class Car_Dealership
-def initialize(manufacturer, make, sold, price)
+def initialize(manufacturer, make, sold, price, status)
     @manufacturer = manufacturer
     @make = make
     @sold = sold
     @price = price
+    @status = status
   end
 
 def to_s
   "This car was manufactured in #{@manufacturer}.  Its make is #{@make}.
-   It was sold in #{@sold}.  It's price was #{@price}."
+   It was sold in #{@sold}.  It's price was #{@price}. This is a #{@status} vehicle."
 end
 
 end
 
-car1 = Car_Dealership.new(2013,"Chevrolet",2017,20000)
+car1 = Car_Dealership.new(2013,"Chevrolet",2017,20000,"used")
 puts car1
