d61412ced917ba55da56a21dfcf2719a077b1763
ndschalk <52681861+ndschalk@users.noreply.github.com>
2019-07-22 20:06:49 -0500

    changed sentence

--- Car_Dealership.rb
+++ Car_Dealership.rb
@@ -1,24 +1,21 @@
 class Car_Dealership
 
-def initialize(make, year_sold, year_sold_price, price, resell)
+def initialize(make, year_sold, year_sold_price, resell, manufacturer)
   @make = make
   @yearsoldprice = year_sold_price
   @yearsold = year_sold
-  @price = price
   @resell = resell
+  @manufacturer = manufacturer
 end
 
 
 def to_s
-  "This car was manufactured in #{@manufacturer}.  Its make is a #{@make}.
-   It was sold in #{@sold} for #{@yearsoldprice}.  It's current list price is #{@price}."
+  "The #{@manufacturer} #{@make} sold for #{@yearsoldprice} in the year of #{@yearsold}, the current resell value is #{@resll}"
  end
 
 end
 
-car1 = Car_Dealership.new(2013,"Chevrolet",2017,20000,"used")
+car1 = Car_Dealership.new("truck", 2000, "$35000", "$5000", "Chevy")
 
-car2 = Car_Dealership.new(2000,"Ford",1999,10000,"new")
 
 puts car1.to_s
-puts car2.to_s
