0b7f00ca2316cca7d37c15334a9d8d649114abd0
Garr22 <bohnertgarrett@gmail.com>
2019-07-25 19:20:05 -0700

    Car Update

--- Car.rb
+++ Car.rb
@@ -1,12 +1,30 @@
 class Car
-  def initialize(make, model, manufacturer)
-    @make = make
-    @model = model
-    @manufacturer = manufacturer
+
+  def initialize(year, make, model, manufacturer)
+    @year = year
+    @make = make.capitalize
+    @model = model.capitalize
+    @manufacturer = manufacturer.capitalize
+  end
+
+  def car_info
+    "#{@year}, #{@make}, #{@model}, #{@manufacturer}"
+  end
+
 end
 
-car1 = Car.new
-car2 = Car.new
-car3 = Car.new
-car4 = Car.new
-car5 = Car.new
+
+car1 = Car.new(2019,"CX-3","Sport", "Mazda")
+puts car1.car_info
+
+car2 = Car.new(2010, "XLS", "Escape", "Ford")
+puts car2.car_info
+
+car3 = Car.new(2016, "Laredo", "Grand Cherokee", "Jeep")
+puts car3.car_info
+
+car4 = Car.new(2018, "Touring", "300","Chrysler")
+puts car4.car_info
+
+car5 = Car.new(2017, "Sedan", "Honda Civic LX","Honda")
+puts car5.car_info
