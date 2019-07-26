27fa43db23d58e91956ea1c825b8f1449458d7c1
Garr22 <bohnertgarrett@gmail.com>
2019-07-25 19:53:42 -0700

    CAr update 2

--- Car.rb
+++ Car.rb
@@ -1,6 +1,7 @@
 class Car
 
-  def initialize(year, make, model, manufacturer)
+  def initialize(price, year, make, model, manufacturer)
+    @price = price
     @year = year
     @make = make.capitalize
     @model = model.capitalize
@@ -8,23 +9,23 @@ class Car
   end
 
   def car_info
-    "#{@year}, #{@make}, #{@model}, #{@manufacturer}"
+    "#{@price},#{@year}, #{@make}, #{@model}, #{@manufacturer}"
   end
 
 end
 
 
-car1 = Car.new(2019,"CX-3","Sport", "Mazda")
+car1 = Car.new(20390, "2019","CX-3","Sport", "Mazda")
 puts car1.car_info
 
-car2 = Car.new(2010, "XLS", "Escape", "Ford")
+car2 = Car.new(32370, "2010", "XLS", "Escape", "Ford")
 puts car2.car_info
 
-car3 = Car.new(2016, "Laredo", "Grand Cherokee", "Jeep")
+car3 = Car.new(32295, "2016", "Laredo", "Grand Cherokee", "Jeep")
 puts car3.car_info
 
-car4 = Car.new(2018, "Touring", "300","Chrysler")
+car4 = Car.new(28995, "2018", "Touring", "300","Chrysler")
 puts car4.car_info
 
-car5 = Car.new(2017, "Sedan", "Honda Civic LX","Honda")
+car5 = Car.new(18740, "2017", "Sedan", "Honda Civic LX","Honda")
 puts car5.car_info
