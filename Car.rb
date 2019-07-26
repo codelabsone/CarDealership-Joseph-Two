1451467d5c5be3b75b0c452f6de48676f90a65ed
Garr22 <bohnertgarrett@gmail.com>
2019-07-25 18:39:39 -0700

    Update on Car.

--- Car.rb
+++ Car.rb
@@ -1,2 +1,12 @@
 class Car
+  def initialize(make, model, manufacturer)
+    @make = make
+    @model = model
+    @manufacturer = manufacturer
 end
+
+car1 = Car.new
+car2 = Car.new
+car3 = Car.new
+car4 = Car.new
+car5 = Car.new
