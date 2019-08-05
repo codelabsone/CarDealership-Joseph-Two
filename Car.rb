4cab901fc57ee2851673f93e5045af7f83abf3a7
ndschalk <52681861+ndschalk@users.noreply.github.com>
2019-08-05 18:54:23 -0500

    Simplify Car

--- Car.rb
+++ Car.rb
@@ -1,31 +1,29 @@
 class Car
 
-  def initialize(price, year, make, model, manufacturer)
+  def initialize(price, year, manufacturer)
     @price = price
     @year = year
-    @make = make.capitalize
-    @model = model.capitalize
     @manufacturer = manufacturer.capitalize
   end
 
   def car_info
-    "#{@price},#{@year}, #{@make}, #{@model}, #{@manufacturer}"
+    "#{@price}, #{@year}, #{@manufacturer}"
   end
 
 end
 
 
-car1 = Car.new(20390, "2019","CX-3","Sport", "Mazda")
+car1 = Car.new(20390, 2019, "Mazda")
 puts car1.car_info
 
-car2 = Car.new(32370, "2010", "XLS", "Escape", "Ford")
+car2 = Car.new(32370, 2010, "Ford")
 puts car2.car_info
 
-car3 = Car.new(32295, "2016", "Laredo", "Grand Cherokee", "Jeep")
+car3 = Car.new(32295, 2016 , "Jeep")
 puts car3.car_info
 
-car4 = Car.new(28995, "2018", "Touring", "300","Chrysler")
+car4 = Car.new(28995, 2018,"Chrysler")
 puts car4.car_info
 
-car5 = Car.new(18740, "2017", "Sedan", "Honda Civic LX","Honda")
+car5 = Car.new(18740, 2017,"Honda")
 puts car5.car_info
