4ca6312621d9bebcd48b3af8941d9399d04ceb35
Garr22 <bohnertgarrett@gmail.com>
2019-08-05 20:04:38 -0700

    Update 8/5

--- Car_Dealership.rb
+++ Car_Dealership.rb
@@ -1,4 +1,22 @@
 require_relative 'Title'
+require_relative 'Car'
 
 deals = Dealership.new("X")
-puts "The name of the dealership is #{deals.title}."
+puts "Dealer Name: #{deals.title}"
+
+puts "------------------------"
+
+car1 = Car.new(20390, 2019, "Mazda")
+puts car1.car_info
+
+car2 = Car.new(32370, 2010, "Ford")
+puts car2.car_info
+
+car3 = Car.new(32295, 2016 , "Jeep")
+puts car3.car_info
+
+car4 = Car.new(28995, 2018,"Chrysler")
+puts car4.car_info
+
+car5 = Car.new(18740, 2017,"Honda")
+puts car5.car_info
