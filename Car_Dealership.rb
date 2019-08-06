7b0adb457a82378795d12c513949e5dc24e7f61b
Garr22 <bohnertgarrett@gmail.com>
2019-08-05 19:53:38 -0700

    Car dealership file change.

--- Car_Dealership.rb
+++ Car_Dealership.rb
@@ -1,21 +1,4 @@
-require_relative 'Car'
-
-class Dealership
-  attr_accessor :title
-
-def initialize(title)
-  @title = title
-end
-
-def add_car(a_car)
-  @car.push(a_car)
-end
-
-def remove_car(a_car)
-  @car.pop(a_car)
-
-end
-end
+require_relative 'Title'
 
 deals = Dealership.new("X")
 puts "The name of the dealership is #{deals.title}."
