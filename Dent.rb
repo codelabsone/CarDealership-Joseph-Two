65e178bcbed0a5dd3cb55256cbafda320f1e62da
ndschalk <52681861+ndschalk@users.noreply.github.com>
2019-08-15 19:53:32 -0500

    modify

--- Dent.rb
+++ Dent.rb
@@ -1,8 +1,12 @@
-reuqire_relative 'Data'
+require_relative 'Data'
 # require_relative "Car"
-# require_relative "Car_Dealership"
 
-module dents
+
+module Dents
 
 def self.make_dent
   rand(500..3000)
+  puts @price - rand(500..3000)
+
+end
+end
