df84ab481e8b5cc7074f5cb1c213bb2e69800d22
ndschalk <52681861+ndschalk@users.noreply.github.com>
2019-08-15 19:00:21 -0500

    Module created

--- Dent.rb
+++ Dent.rb
@@ -2,27 +2,7 @@ reuqire_relative 'Data'
 # require_relative "Car"
 # require_relative "Car_Dealership"
 
-class Sell_Price
+module dents
 
-   def initialize(base=5000)
-        @base = base
-    end
-
-def dents
-  @base -= 500
-  puts "#{@base}"
-
-def stains
-  @base -= 200
-  puts "#{@base}"
-
-def accident
-  @base -= @base
-  puts "#{@base}"
-    end
-  end
-end
-end
-
-car = Sell_Price.new(500)
-car.dents
+def self.make_dent
+  rand(500..3000)
